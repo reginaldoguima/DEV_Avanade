acdce3647a993aaac994967824c51332e0d164b9
reginaldoguima <reginaldo.olinda@gmail.com>
2020-12-29 22:34:44 -0300

    Sincroização de Codigos 2

--- NET/Backend/curso/curso.api/Startup.cs
+++ NET/Backend/curso/curso.api/Startup.cs
@@ -38,6 +38,8 @@ namespace curso.api
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ NET/Design_PAtterns/Texto.md
@@ -0,0 +1,4 @@
+# Aplicando Design Patterns na Prática
+
+### O texto é apenas para deixar claro que não implementei o desafio. Entendi que a implementação era para pessoas com mais casca do que eu, de repente um pleno ou até senior. Eu AINDA não tenho essa capacidade, mas apenas AINDA.
+
